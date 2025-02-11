fa73a40d1aa4efe6e36ec67872bdbc2945e63e18
Christian Wedema <61030366+shippingport@users.noreply.github.com>
2025-02-11 09:01:18 +0100

    Update Remove-EmptyCalendarFolders.ps1

--- Scripts/Graph/Remove-EmptyCalendarFolders.ps1
+++ Scripts/Graph/Remove-EmptyCalendarFolders.ps1
@@ -1,14 +1,14 @@
-# This script removals all user calendars that have no content.
-# First connect to Graph with the following scropes:
+# This script removes all the calendars that have no content in a given user's Exchange mailbox.
+# First connect to Graph with the following scopes:
 # Connect-MgGraph -Scopes "User.Read.All" , "Calendars.ReadWrite"
 
-$user = Read-Host "Welke user?"
+$user = Read-Host "Which user [id]?"
 $agendaFolders = Get-MailboxFolderStatistics -Identity $user -FolderScope Calendar | Select-Object ItemsInFolderAndSubfolders,Name
 $userId = Get-MgUser -UserId $user | Select-Object Id
 
 foreach($agenda in $agendaFolders) {
     If($agenda.ItemsInFolderAndSubfolders -eq 0) {
-        Write-Host "Lege map: " $agenda.Name
+        Write-Host "Found empty calendar: " $agenda.Name
         $switch = Read-Host "Remove this calendar?"
         If($switch -eq "Y|y") {
             $AgendaGraphId = Get-MgUserCalendar -UserId $user | Where-Object -Propert Name -eq $agenda.Name | Select-Object Id
